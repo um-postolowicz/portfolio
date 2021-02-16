f737637c346e2deaf80259c8622e037727f26f41
Urszula <u.postolowicz@gmail.com>
2021-02-16 16:24:33 +0100

    Add favicon, page title and main (introduction) page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import "./App.css";
+import MainPage from "./pages/MainPage";
+import "./styles/style.css";
 
 function App() {
-  return <div className="App">Hey</div>;
+  return (
+    <>
+      <div className="wrapper">
+        <MainPage />
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,7 +1,20 @@
 import React from "react";
+import avatar from "../images/avatar.svg";
 
 const MainPage = () => {
-  return <div>Urszula Maria Postołowicz</div>;
+  return (
+    <>
+      <section className="mainPage">
+        <div className="introduction">
+          <h1 className="welcome">Hi!</h1>
+          <h1 className="name">My name is Urszula Maria Postołowicz.</h1>
+          <h1 className="profession">I'm a FrontEnd Developer.</h1>
+          <h2 className="invitation">Be my guest!</h2>
+        </div>
+        <img src={avatar} alt="avatar" className="avatar" />
+      </section>
+    </>
+  );
 };
 
 export default MainPage;
