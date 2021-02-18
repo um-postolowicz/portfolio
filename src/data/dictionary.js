fd9e8e9455fd7ea9e1f328b929ea2e0e24b357d9
Urszula <u.postolowicz@gmail.com>
2021-02-18 08:43:17 +0100

    Create "About" component

--- src/data/dictionary.js
+++ src/data/dictionary.js
@@ -4,11 +4,15 @@ export const dictionary = {
     name: "Nazywam się Urszula Maria Postołowicz",
     profession: "Jestem FrontEnd Developerem",
     invitation: "Zapraszam na moją stronę!",
+    aboutTitle: "O mnie",
+    about: "Opis",
   },
   english: {
     welcome: "Hi!",
     name: "My name is Urszula Maria Postołowicz",
     profession: "I'm a FrontEnd Developer",
     invitation: "Be my guest!",
+    aboutTitle: "About me",
+    about: "Description",
   },
 };
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -6,13 +6,14 @@ const MainPage = () => {
   return (
     <Consumer>
       {({ label }) => {
+        const { welcome, name, profession, invitation } = label;
         return (
           <section className="mainPage">
             <div className="introduction">
-              <h1 className="welcome">{label.welcome}</h1>
-              <h1 className="name">{label.name}</h1>
-              <h1 className="profession">{label.profession}</h1>
-              <h2 className="invitation">{label.invitation}</h2>
+              <h1 className="welcome">{welcome}</h1>
+              <h1 className="name">{name}</h1>
+              <h1 className="profession">{profession}</h1>
+              <h2 className="invitation">{invitation}</h2>
             </div>
             <div className="avatar">
               <img src={avatar} alt="avatar" className="avatarImg" />
