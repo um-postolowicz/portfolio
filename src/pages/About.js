2ef3e4f8d9d9d763b40058e0febdf2d6f6ee7dd3
Urszula <u.postolowicz@gmail.com>
2021-02-22 11:27:24 +0100

    Create basic style for "About" page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Consumer } from "../data/LanguageContext";
-import border from "../images/about-border.svg";
 
 const About = () => {
   return (
@@ -9,37 +8,11 @@ const About = () => {
         const { about, aboutTitle } = label;
         return (
           <section className="about">
-            <div className="borderDiv borderTopLeft">
-              <img
-                src={border}
-                alt="Floral border"
-                className="border topLeft"
-              />
-            </div>
-            <div className="borderDiv borderTopRight">
-              <img
-                src={border}
-                alt="Floral border"
-                className="border topRight"
-              />
-            </div>
-            <div className="borderDiv borderBottomLeft">
-              <img
-                src={border}
-                alt="Floral border"
-                className="border bottomLeft"
-              />
-            </div>
-            <div className="borderDiv borderBottomRight">
-              <img
-                src={border}
-                alt="Floral border"
-                className="border bottomRight"
-              />
-            </div>
-            <div className="aboutTxt">
-              <h3 className="aboutHeader">{aboutTitle}</h3>
-              <p className="aboutDesc">{about}</p>
+            <div className="aboutBorder">
+              <div className="aboutTxt">
+                <h3 className="aboutHeader">{aboutTitle}</h3>
+                <p className="aboutDesc">{about}</p>
+              </div>
             </div>
           </section>
         );
