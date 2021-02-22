354f6567d563e8884b19449753581012650e146a
Urszula <u.postolowicz@gmail.com>
2021-02-22 09:37:27 +0100

    Add basic style for "Projects" section

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,9 +3,9 @@ import { Consumer } from "../data/LanguageContext";
 import Project from "../components/Project";
 
 import portfolioImg from "../images/avatar.svg";
-import rateMovieImg from "../images/rateamovie.PNG";
-import toDoImg from "../images/todolist.PNG";
-import calculatorImg from "../images/calculatorSmall.PNG";
+import rateMovieImg from "../images/moviesFav.PNG";
+import toDoImg from "../images/todoFav.png";
+import calculatorImg from "../images/calculatorFav.png";
 
 const Projects = () => {
   return (
@@ -25,46 +25,48 @@ const Projects = () => {
         return (
           <section className="projects">
             <h3 className="projectsTitle">{projectsTitle}</h3>
-            <Project
-              alt="Portfolio IMG"
-              descClassName="portfolioDesc"
-              descTxt={portfolioTxt}
-              imgClassName="portfolioImg"
-              imgSrc={portfolioImg}
-              mainClassName="portfolio"
-              projectName={portfolioTitle}
-              titleClassName="portfolioTitle"
-            />
-            <Project
-              alt="Rate a movie IMG"
-              descClassName="rateMovieDesc"
-              descTxt={rateMovieTxt}
-              imgClassName="rateMovieImg"
-              imgSrc={rateMovieImg}
-              mainClassName="rateMovie"
-              projectName={rateMovieTitle}
-              titleClassName="rateMovieTitle"
-            />
-            <Project
-              alt="To Do IMG"
-              descClassName="toDoDesc"
-              descTxt={toDoTxt}
-              imgClassName="toDoImg"
-              imgSrc={toDoImg}
-              mainClassName="toDo"
-              projectName={toDoTitle}
-              titleClassName="toDoTitle"
-            />
-            <Project
-              alt="The Calculator IMG"
-              descClassName="calculatorDesc"
-              descTxt={calculatorTxt}
-              imgClassName="calculatorImg"
-              imgSrc={calculatorImg}
-              mainClassName="calculator"
-              projectName={calculatorTitle}
-              titleClassName="calculatorTitle"
-            />
+            <div className="projectsContainer">
+              <Project
+                alt="Portfolio IMG"
+                descClassName="portfolioDesc"
+                descTxt={portfolioTxt}
+                imgClassName="portfolioImg"
+                imgSrc={portfolioImg}
+                mainClassName="portfolio project"
+                projectName={portfolioTitle}
+                titleClassName="portfolioTitle"
+              />
+              <Project
+                alt="Rate a movie IMG"
+                descClassName="rateMovieDesc"
+                descTxt={rateMovieTxt}
+                imgClassName="rateMovieImg"
+                imgSrc={rateMovieImg}
+                mainClassName="rateMovie project"
+                projectName={rateMovieTitle}
+                titleClassName="rateMovieTitle"
+              />
+              <Project
+                alt="To Do IMG"
+                descClassName="toDoDesc"
+                descTxt={toDoTxt}
+                imgClassName="toDoImg"
+                imgSrc={toDoImg}
+                mainClassName="toDo project"
+                projectName={toDoTitle}
+                titleClassName="toDoTitle"
+              />
+              <Project
+                alt="The Calculator IMG"
+                descClassName="calculatorDesc"
+                descTxt={calculatorTxt}
+                imgClassName="calculatorImg"
+                imgSrc={calculatorImg}
+                mainClassName="calculator project"
+                projectName={calculatorTitle}
+                titleClassName="calculatorTitle"
+              />
+            </div>
           </section>
         );
       }}
