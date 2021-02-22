2c45381b09858c08ea762177ff9c9f2becf32c48
Urszula <u.postolowicz@gmail.com>
2021-02-22 12:42:38 +0100

    Add links to contact section

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -12,14 +12,23 @@ const Contact = () => {
         return (
           <section className="contact">
             <h3 className="contactTitle">{contactTitle}</h3>
-            <div className="email contactTile">
+            <div
+              className="email contactTile"
+              onClick={() => (window.location.href = "https://www.gmail.com")}
+            >
               <h4 className="emailTitle">Email</h4>
               <div className="icon">
                 <HiOutlineMailOpen />
               </div>
               <p className="emailTxt">u.postolowicz@gmail.com</p>
             </div>
-            <div className="facebook contactTile">
+            <div
+              className="facebook contactTile"
+              onClick={() =>
+                (window.location.href =
+                  "https://www.facebook.com/profile.php?id=100004203695582")
+              }
+            >
               <h4 className="facebookTitle">Facebook</h4>
               <div className="icon">
                 <FaFacebookSquare />
@@ -33,7 +42,12 @@ const Contact = () => {
               </div>
               <p className="linkedinTxt">Urszula Maria Postołowicz</p>
             </div>
-            <div className="github contactTile">
+            <div
+              className="github contactTile"
+              onClick={() =>
+                (window.location.href = "https://github.com/um-postolowicz")
+              }
+            >
               <h4 className="githubTitle">GitHub</h4>
               <div className="icon">
                 <FaGithubSquare />
